4a57b959a5719058486792b7112db6763e56fcb3
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2024-07-20 23:49:53 +0200

    add cards for navigation

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,5 +1,6 @@
 import "../styles/home.css";
-import kitchen from "../assets/kitchen.jpg"
+import kitchen from "../assets/kitchen.jpg";
+import food from "../assets/food.jpeg";
 
 const loadHome = () => {
     const content = document.querySelector("#content");
@@ -17,13 +18,37 @@ const loadHome = () => {
     credit.textContent = "Picture from `KITCHENS Hamburg`";
 
     //create cards
+    const menuCardDiv = document.createElement("div");
+    menuCardDiv.setAttribute("class","menuCardDiv");
+    const menuCard = document.createElement("div");
+    menuCard.setAttribute("class","menuCard");
+    const foodPicture = document.createElement("img");
+    const menuP = document.createElement("p");
+    const menuH2 = document.createElement("h2");
+    const menuText = document.createElement("div");
+    const menuBtn = document.createElement("button");
+    menuBtn.textContent = "Zur Speisekarte"
+    foodPicture.src = food;
+    menuH2.textContent = "Unsere Speisekarte";
+    menuP.textContent = "Für uns ist Qualität das A & O. Sie finden in unserer Speisekarte eine ganze Reihe Auswahl, von Steaks und Pasta bis hin zu vegetarischen Gerichten. Lassen Sie sich begeistern und schauen sie rein";
+
+    menuText.appendChild(menuH2);
+    menuText.appendChild(menuP);
+    menuText.appendChild(menuBtn);
+    menuCard.appendChild(foodPicture);
+    menuCard.appendChild(menuText);
+    menuCardDiv.appendChild(menuCard);
+
+
+
 
     //append to image container
     imageContainer.appendChild(h1);
     imageContainer.appendChild(credit);
 
     //append to content
-    content.appendChild(imageContainer)
+    content.appendChild(imageContainer);
+    content.appendChild(menuCardDiv)
 }
 
 export {loadHome}
